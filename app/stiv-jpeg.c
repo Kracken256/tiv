5ac44f725a81d86134cb4a6e544701921ae28e0a
Wesley Jones <wesjones2004@gmail.com>
2024-06-07 15:59:53 -1000

    Reformat

--- app/stiv-jpeg.c
+++ app/stiv-jpeg.c
@@ -1,42 +1,47 @@
 /* tiv - terminal image viewer - copyleft 2013-2015 - pancake */
 
-#include <string.h>
 #include <stdlib.h>
-
 #include <stiv.h>
+#include <string.h>
 
 int main(int argc, const char** argv) {
-    stivctx_t* ctx;
-    uint32_t width = 80;
-    stiv_mode mode = STIV_MODE_RGB;
+  stivctx_t* ctx;
+  uint32_t width = 80;
+  stiv_mode mode = STIV_MODE_RGB;
 
-    if (argc < 2) {
-        printf("stiv-jpeg . suckless terminal image viewer\n"
-            "Usage: stiv [image] [width] [mode]\n"
-            "Modes: [ascii,ansi,grey,256,rgb]\n");
-        return 1;
-    }
+  if (argc < 2) {
+    printf(
+        "stiv-jpeg . suckless terminal image viewer\n"
+        "Usage: stiv [image] [width] [mode]\n"
+        "Modes: [ascii,ansi,grey,256,rgb]\n");
+    return 1;
+  }
 
-    if (argc > 2) {
-        width = atoi(argv[2]);
-    }
+  if (argc > 2) {
+    width = atoi(argv[2]);
+  }
 
-    if (argc > 3) {
-        if (!strcmp(argv[3], "ascii")) mode = STIV_MODE_ASCII;
-        else if (!strcmp(argv[3], "ansi")) mode = STIV_MODE_ANSI;
-        else if (!strcmp(argv[3], "grey")) mode = STIV_MODE_GREY;
-        else if (!strcmp(argv[3], "256")) mode = STIV_MODE_256;
-        else if (!strcmp(argv[3], "rgb")) mode = STIV_MODE_RGB;
-    }
+  if (argc > 3) {
+    if (!strcmp(argv[3], "ascii"))
+      mode = STIV_MODE_ASCII;
+    else if (!strcmp(argv[3], "ansi"))
+      mode = STIV_MODE_ANSI;
+    else if (!strcmp(argv[3], "grey"))
+      mode = STIV_MODE_GREY;
+    else if (!strcmp(argv[3], "256"))
+      mode = STIV_MODE_256;
+    else if (!strcmp(argv[3], "rgb"))
+      mode = STIV_MODE_RGB;
+  }
 
-    if (!(ctx = stiv_from_jpeg(argv[1], width, 0, mode))) {
-        printf("Failed to load image\n");
-        return 1;
-    }
+  if (!(ctx = stiv_from_jpeg(argv[1], width, 0, mode))) {
+    printf("Failed to load image\n");
+    return 1;
+  }
 
-    stiv_display(ctx);
+  stiv_display(ctx);
 
-    stiv_free(ctx);
+  stiv_free(ctx);
 
-    return 0;
+  return 0;
 }
--- libstiv/src/stiv.h
+++ libstiv/src/stiv.h
@@ -8,57 +8,57 @@ extern "C" {
 #endif
 
 #include <stdint.h>
-#include <stdio.h>  
+#include <stdio.h>
 
-    /// @brief The stiv context.
-    typedef struct stivctx_t stivctx_t;
+/// @brief The stiv context.
+typedef struct stivctx_t stivctx_t;
 
-    typedef enum {
-        STIV_MODE_ASCII,
-        STIV_MODE_ANSI,
-        STIV_MODE_GREY,
-        STIV_MODE_256,
-        STIV_MODE_RGB
-    } stiv_mode;
+typedef enum {
+  STIV_MODE_ASCII,
+  STIV_MODE_ANSI,
+  STIV_MODE_GREY,
+  STIV_MODE_256,
+  STIV_MODE_RGB
+} stiv_mode;
 
-    /// @brief Create a stiv context from a raw RGB24 buffer
-    /// @param buffer The raw RGB24 buffer
-    /// @param buffer_size The size of the buffer in bytes
-    /// @param width The width of the image in pixels
-    /// @param height The height of the image in pixels
-    /// @param mode The mode to use for rendering
-    /// @return The stiv context
-    /// @note The buffer will be copied and managed internally.
-    /// @note The image will not be displayed until `stiv_display` 
-    ///       is called with the context.
-    /// @warning This function will return NULL on failure.
-    stivctx_t* stiv_create(const uint8_t* buffer, size_t buffer_size,
-        uint32_t width, uint32_t height, stiv_mode mode);
+/// @brief Create a stiv context from a raw RGB24 buffer
+/// @param buffer The raw RGB24 buffer
+/// @param buffer_size The size of the buffer in bytes
+/// @param width The width of the image in pixels
+/// @param height The height of the image in pixels
+/// @param mode The mode to use for rendering
+/// @return The stiv context
+/// @note The buffer will be copied and managed internally.
+/// @note The image will not be displayed until `stiv_display`
+///       is called with the context.
+/// @warning This function will return NULL on failure.
+stivctx_t* stiv_create(const uint8_t* buffer, size_t buffer_size,
+                       uint32_t width, uint32_t height, stiv_mode mode);
 
-    /// @brief Create a stiv context from a JPEG file
-    /// @param jpeg_filepath The path to the JPEG file
-    /// @param width The width of the image in pixels
-    /// @param height The height of the image in pixels
-    /// @param mode The mode to use for rendering
-    /// @return The stiv context
-    /// @note The image will not be displayed until `stiv_display` 
-    ///       is called with the context.
-    /// @warning This function will return NULL on failure.
-    stivctx_t* stiv_from_jpeg(const char* jpeg_filepath, uint32_t width,
-        uint32_t height, stiv_mode mode);
+/// @brief Create a stiv context from a JPEG file
+/// @param jpeg_filepath The path to the JPEG file
+/// @param width The width of the image in pixels
+/// @param height The height of the image in pixels
+/// @param mode The mode to use for rendering
+/// @return The stiv context
+/// @note The image will not be displayed until `stiv_display`
+///       is called with the context.
+/// @warning This function will return NULL on failure.
+stivctx_t* stiv_from_jpeg(const char* jpeg_filepath, uint32_t width,
+                          uint32_t height, stiv_mode mode);
 
-    /// @brief Release the resources associated within a stiv context
-    /// @param ctx The stiv context
-    /// @note If `!ctx`, this is a no-op.
-    void stiv_free(stivctx_t* ctx);
+/// @brief Release the resources associated within a stiv context
+/// @param ctx The stiv context
+/// @note If `!ctx`, this is a no-op.
+void stiv_free(stivctx_t* ctx);
 
-    /// @brief Display the image associated with a stiv context
-    /// @param ctx The stiv context
-    /// @note If `!ctx`, this is a no-op.
-    void stiv_display(stivctx_t* ctx);
+/// @brief Display the image associated with a stiv context
+/// @param ctx The stiv context
+/// @note If `!ctx`, this is a no-op.
+void stiv_display(stivctx_t* ctx);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // __STIV_H__
+#endif  // __STIV_H__
